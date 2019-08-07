6367bcc725af1829a32ebc91ff8bea0aa74f9113
cmking54 <cmking5496@gmail.com>
2019-08-07 16:52:32 -0400

    More refactoring

--- OperatorShowcase.java
+++ OperatorShowcase.java
@@ -10,15 +10,17 @@ public class OperatorShowcase {
             operator.run_tests();
         }
     }
-    public static void main(String[] args) {}
+    public static void main(String[] args) {
+        new OperatorShowcase().run_all_tests();
+    }
 
     public abstract class Operator {
         protected int random_operand() {
-            return new Random().nextInt(100); //can be anything really, I am choosing integers for simplicity
+            return new Random().nextInt(10); //can be anything really, I am choosing integers for simplicity
         }
         abstract void run_tests();
     }
-    class ArithmeticOperator extends Operator {
+    public class ArithmeticOperator extends Operator {
         String[] symbols;
         public ArithmeticOperator() {
             symbols = new String[]{"+", "-", "*", "/", "%", "++", "--"};
@@ -27,52 +29,58 @@ public class OperatorShowcase {
             return a + b; // simply known as plus
         }
         private void addition_test() {
-            System.out.println("\n======= ADDITION TEST ======");
+            System.out.println("\n======= ADDITION TEST =======");
             int operand_1 = random_operand();
             int operand_2 = random_operand();
-            System.out.println(operand_1);
-            System.out.println(operand_2);
-            int sum_1 = 320 + 60;
-            System.out.println(sum_1);
-            int sum_2 = operand_1 + 201;
-            System.out.println(sum_2);
+            //System.out.println("Operand 1: " + operand_1);
+            //System.out.println("Operand 2: " + operand_2);
+            int sum_1 = 32 + 6;
+            System.out.println("32 plus 6 is " + sum_1);
+            int sum_2 = operand_1 + 2;
+            System.out.println(operand_1 + " plus 2 is " + sum_2);
             int sum_3 = operand_1 + operand_2;
-            System.out.println(sum_3);
-            System.out.println("===============================");
+            System.out.println(operand_1 + " plus " + operand_2 + " is " + sum_3);
+            System.out.println("=============================");
         }
         private int modulus_operator(int a, int b){
             return a % b; // "wraps around"
         }
         private void modulus_test() {
-            System.out.println("\n====== MOD TEST ======");
+            System.out.println("\n========= MOD TEST ==========");
             int operand_1 = random_operand();
-            int operand_2 = random_operand();
-            System.out.println(operand_1);
-            System.out.println(operand_2);
+            int operand_2 = random_operand() + 1;
+            //System.out.println(operand_1);
+            //System.out.println(operand_2);
 
-            int result_1 = operand_1 % operand_2;
-            System.out.println(result_1);
+            int result_1 = (operand_1*12) % operand_2;
+            System.out.println(operand_1*12 + " mod " + operand_2 + " is " + result_1);
             int result_2 = 0 % 10;
-            System.out.println(result_2);
+            System.out.println("0 mod 10 is " + result_2);
             int result_3 = 10 % 1;
-            System.out.println(result_3);
-            System.out.println("===============================");
+            System.out.println("10 mod 1 is " + result_3);
+            System.out.println("=============================");
         }
         private int increment_operator(int a){
             return a++; // adds one, then replaces
         }
         private void increment_test() {
-            System.out.println("\n====== INC TEST ======");
+            System.out.println("\n========= INC TEST ==========");
             int operand_1 = random_operand();
-            System.out.println(operand_1);
+            System.out.println("Before increment, the starting value is " + operand_1);
             operand_1++;
-            System.out.println(operand_1);
+            System.out.println("After increment, it was updated to " + operand_1);
             int result_1 = operand_1++;
-            System.out.println(operand_1);
-            System.out.println(result_1);
-            System.out.println("===============================");
+            System.out.println("Here is incrementing and storing written on the same line");
+            System.out.println("The end result is it being stored and then updated since the ++ comes after");
+            System.out.println("The result is " + result_1 + " and the updating value is " + operand_1);
+            System.out.println("==============================");
+        }
+        private void print_symbols() {
+            System.out.println("\n========================== ARITHMETIC ===========================");
+            System.out.println("Some operators for the Arithmetic category are: " + String.join(", ", symbols));
         }
         public void run_tests() {
+            print_symbols();
             addition_test();
             modulus_test();
             increment_test();
